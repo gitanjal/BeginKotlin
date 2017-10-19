1a0468e33983140fa8af2e16abc858283526212b
gitudrebel <gitudrebel94@gmail.com>
2017-10-19 23:05:30 +0530

    Start new activity

---
+++ app/src/main/java/com/droidmonk/beginkotlin/ActivityB.kt
@@ -0,0 +1,15 @@
+package com.droidmonk.beginkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityB : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+
+
+    }
+}
--- app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
+++ app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.droidmonk.beginkotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,9 +13,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnChangeText=findViewById(R.id.btn_change_text) as Button;
+        val btnLoadActivityB=findViewById(R.id.btn_load_activity_b) as Button;
         val tvHello=findViewById(R.id.tv_hello) as TextView;
+
         btnChangeText.setOnClickListener {
             tvHello.text="Yay! I am a Kotlin expert";
+        }
+
+        btnLoadActivityB.setOnClickListener{
+            val intent= Intent(this,ActivityB::class.java);
+            startActivity(intent);
         }
     }
 }
