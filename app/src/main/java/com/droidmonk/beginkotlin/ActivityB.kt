42dfcbecb65d3d1ed5660e9fe3d658ceda454226
gitudrebel <gitudrebel94@gmail.com>
2017-10-19 23:22:28 +0530

    Using extensions plugin

--- app/src/main/java/com/droidmonk/beginkotlin/ActivityB.kt
+++ app/src/main/java/com/droidmonk/beginkotlin/ActivityB.kt
@@ -2,14 +2,15 @@ package com.droidmonk.beginkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_b.tv_msg
 
 class ActivityB : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
 
+        tv_msg.text="Activity B too using Android Extension Plugin"
 
     }
 }
--- app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
+++ app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,15 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnChangeText=findViewById(R.id.btn_change_text) as Button;
-        val btnLoadActivityB=findViewById(R.id.btn_load_activity_b) as Button;
-        val tvHello=findViewById(R.id.tv_hello) as TextView;
 
-        btnChangeText.setOnClickListener {
-            tvHello.text="Yay! I am a Kotlin expert";
+        btn_change_text.setOnClickListener {
+            tv_hello.text="Yay! I am a Kotlin expert";
         }
 
-        btnLoadActivityB.setOnClickListener{
+        btn_load_activity_b.setOnClickListener{
             val intent= Intent(this,ActivityB::class.java);
             startActivity(intent);
         }
