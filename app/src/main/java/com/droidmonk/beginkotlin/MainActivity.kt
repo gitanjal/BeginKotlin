2a701a0ca4d49c97d24c5578ac74ffdc51931f37
gitudrebel <gitudrebel94@gmail.com>
2017-10-21 21:35:25 +0530

    Experiment

--- app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
+++ app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.droidmonk.beginkotlin
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 
@@ -13,23 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnChangeText=findViewById(R.id.btn_change_text) as Button;
-        val btnLoadActivityB=findViewById(R.id.btn_load_activity_b) as Button;
-        val tvHello=findViewById(R.id.tv_hello) as TextView;
+        val btnChangeText = findViewById(R.id.btn_change_text) as Button
+        val btnLoadActivityB = findViewById(R.id.btn_load_activity_b) as Button
+        val tvHello = findViewById(R.id.tv_hello) as TextView;
         btnChangeText.setOnClickListener {
-            tvHello.text="Yay! I am a Kotlin expert";
+            tvHello.text = "Yay! I am a Kotlin expert";
         }
-      /*
-      //set onclick listener the traditional way
-      btnChangeText.setOnClickListener(object:View.OnClickListener {
-            override fun onClick(p0: View?) {
-                tvHello.text="Yay! I am a Kotlin expert, but the traditional way";
-            }
-        })
-*/
-        btnLoadActivityB.setOnClickListener{
-            val intent= Intent(this,ActivityB::class.java);
+
+        btnLoadActivityB.setOnClickListener {
+            val intent = Intent(this, ActivityB::class.java);
             startActivity(intent);
         }
     }
 }
+
