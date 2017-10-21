1322583a38bfd52116f84f382f0d7b2a0fdf2060
gitudrebel <gitudrebel94@gmail.com>
2017-10-21 15:41:23 +0530

    onclick listener traditional commented

--- app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
+++ app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.droidmonk.beginkotlin
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -15,11 +16,17 @@ class MainActivity : AppCompatActivity() {
         val btnChangeText=findViewById(R.id.btn_change_text) as Button;
         val btnLoadActivityB=findViewById(R.id.btn_load_activity_b) as Button;
         val tvHello=findViewById(R.id.tv_hello) as TextView;
-
         btnChangeText.setOnClickListener {
             tvHello.text="Yay! I am a Kotlin expert";
         }
-
+      /*
+      //set onclick listener the traditional way
+      btnChangeText.setOnClickListener(object:View.OnClickListener {
+            override fun onClick(p0: View?) {
+                tvHello.text="Yay! I am a Kotlin expert, but the traditional way";
+            }
+        })
+*/
         btnLoadActivityB.setOnClickListener{
             val intent= Intent(this,ActivityB::class.java);
             startActivity(intent);
