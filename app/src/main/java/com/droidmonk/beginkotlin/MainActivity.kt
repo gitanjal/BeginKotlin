837e1ae6f4c51a90abd059efa7d87096c509198a
gitudrebel <gitudrebel94@gmail.com>
2017-11-01 09:15:20 +0530

    random

--- app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
+++ app/src/main/java/com/droidmonk/beginkotlin/MainActivity.kt
@@ -3,7 +3,6 @@ package com.droidmonk.beginkotlin
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
